094145028338ee16f9888c24f7eb2bd62d7453ca
cwhinfrey <cwhinfrey@gmail.com>
2022-08-19 17:53:58 -0700

    Add relay payment system

--- contracts/HopMessageBridge.sol
+++ contracts/HopMessageBridge.sol
@@ -3,11 +3,14 @@ pragma solidity ^0.8.2;
 
 import "./utils/Lib_MerkleTree.sol";
 
+// attach the bundle fee and bundle relayer to the root and reward them when it is set.
+
 interface IHopMessageReceiver {
     function receiveMessageBundle(
         bytes32 bundleRoot,
         uint256 bundleValue,
-        uint256 destinationChainId
+        uint256 toChainId,
+        address bundleRelayerAddress
     ) external payable;
 }
 
@@ -32,38 +35,82 @@ contract HopMessageBridge {
     // destination chain Id -> pending message Ids
     mapping(uint256 => bytes32[]) public pendingMessageIdsForChainId;
     mapping(uint256 => uint256) public pendingValue;
-    mapping(uint256 => uint256) public pendingBundleRelayerReward;
+    mapping(uint256 => uint256) public pendingBundleFees;
     mapping(bytes32 => Bundle) bundles;
     mapping(bytes32 => bool) relayedMessage;
+    mapping(uint256 => uint256) routeMessageFee; // ToDo: Add setter
+    mapping(uint256 => uint256) routeMaxBundleMessages;
 
-    function sendMessage(uint256 destinationChainId, address to, bytes calldata message) external payable {
+    function sendMessage(uint256 toChainId, address to, bytes calldata message, uint256 value) external payable {
+        // Require msg.value > bundleFee
+        // Require bundleFee > minBundleFee
+        uint256 routeFee = routeMessageFee[toChainId];
+        uint256 rquiredValue = routeFee + value;
+        require(rquiredValue == msg.value, "MSG_BRG: Incorrect msg.value");
+
         bytes32 messageId = getMessageId(msg.sender, to, msg.value, message);
-        pendingMessageIdsForChainId[destinationChainId].push(messageId);
-        pendingValue[destinationChainId] = pendingValue[destinationChainId] + msg.value;
+        bytes32 storage pendingMessageIds = pendingMessageIdsForChainId[toChainId];
+        pendingMessageIds.push(messageId);
+
+        pendingValue[toChainId] = pendingValue[toChainId] + msg.value;
+        pendingBundleFees[toChainId] = pendingBundleFees[toChainId] + bundleFee;
+
+        uint256 maxBundleMessages = routeMaxBundleMessages[toChainId];
+
+        if (pendingMessageIds.length >= routeMaxBundleMessages) {
+            _commitMessageBundle(toChainId);
+        }
     }
 
-    function commitMessageBundle(uint256 destinationChainId) external {
-        uint256 bundleValue = pendingValue[destinationChainId];
-        bytes32[] storage pendingMessages = pendingMessageIdsForChainId[destinationChainId];
+    function _commitMessageBundle(uint256 toChainId) private {
+        bytes32[] storage pendingMessages = pendingMessageIdsForChainId[toChainId];
         bytes32 bundleRoot = Lib_MerkleTree.getMerkleRoot(pendingMessages);
+        uint256 bundleValue = pendingValue[toChainId];
+        uint256 pendingFees = pendingBundleFees[toChainId];
 
-        IHopMessageReceiver bridge = bridgeForChainId[destinationChainId];
-        bridge.receiveMessageBundle{value: bundleValue}(bundleRoot, bundleValue, destinationChainId);
+        IHopMessageReceiver bridge = bridgeForChainId[toChainId];
+        bridge.receiveMessageBundle{value: bundleValue}(
+            bundleRoot,
+            bundleValue,
+            pendingFees,
+            getChainId(),
+            toChainId,
+            now
+        );
     }
 
+    // L1 only
+    mapping(uint256 => uint256) exitTime;
+    uint256 relayWindow;
     function receiveMessageBundle(
         bytes32 bundleRoot,
         uint256 bundleValue,
-        uint256 destinationChainId
+        uint256 bundleFees,
+        uint256 fromChainId,
+        uint256 toChainId,
+        address bundleRelayerAddress,
+        uint256 commitTime
     )
         external
         payable
     {
-        if (destinationChainId == getChainId()) {
-            bytes32 bundleId = keccak256(abi.encodePacked(bundleRoot, bundleValue, destinationChainId));
+        require(now >= relayWindowStart, "MSG_BRG: Relay window not started");
+
+        // distribute bundle reward if msg.sender == bundleRelayerAddress || block.timestamp > (commitTime + protectedRelayTime)
+        if (toChainId == getChainId()) {
+            bytes32 bundleId = keccak256(abi.encodePacked(bundleRoot, bundleValue, toChainId));
             bundles[bundleId] = Bundle(bundleRoot, bundleValue);
         } else {
             // forward root to destination
+        }
+
+        uint256 relayWindowStart = commitTime + exitTime[fromChainId];
+        uint256 relayWindowEnd = relayWindowStart + relayWindow;
+        uint256 relayReward = 0;
+        if (now > relayWindowEnd) {
+            relayReward = bundleFees;
+        } else if (now >= relayWindowStart) {
+            relayReward = (now - relayWindowStart) * bundleFees / relayWindow;
         }
     }
 
@@ -112,8 +159,21 @@ contract HopMessageBridge {
         return block.chainid;
     }
 
-    function getMessageId(address from, address to, uint256 value, bytes memory message) public returns (bytes32) {
+    function getMessageId(
+        address from,
+        address to,
+        uint256 value,
+        bytes memory message
+    )
+        public
+        pure
+        returns (bytes32)
+    {
         return keccak256(abi.encode(from, to, value, message));
+    }
+
+    function getRouteId(uint256 toChainId) public pure returns (bytes32) {
+        return keccak256(getChainId(), toChainId);
     }
 
     function xDomainMessageSender() public view returns (address) {
