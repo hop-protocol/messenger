44bad31b84073553576311b9867d2f3a9cbb1022
cwhinfrey <cwhinfrey@gmail.com>
2022-09-01 23:40:23 -0700

    Separate out HubMessageBridge

---
+++ contracts/HubMessageBridge.sol
@@ -0,0 +1,74 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.2;
+
+import "./utils/Lib_MerkleTree.sol";
+import "./MessageBridge.sol";
+
+// attach the bundle fee and bundle relayer to the root and reward them when it is set.
+
+interface ISpokeMessageBridge {
+    function receiveMessageBundle(bytes32 bundleRoot, uint256 bundleValue, uint256 fromChainId) external payable;
+    function forwardMessage(address to, address from, bytes calldata message) external payable;
+}
+
+contract HubMessageBridge is MessageBridge {
+    mapping(uint256 => IHopMessageReceiver) bridgeForChainId;
+    mapping(uint256 => uint256) exitTime;
+    uint256 relayWindow;
+
+    function sendMessage(
+        uint256 toChainId,
+        address to,
+        bytes calldata message,
+        uint256 value
+    )
+        external
+        payable
+    {
+        require(value == msg.value, "MSG_BRG: Incorrect msg.value");
+
+        ISpokeMessageBridge spokeBridge = bridgeForChainId[toChainId];
+        spokeBridge.forwardMessage{value: value}(to, msg.sender, message);
+    }
+
+    function receiveOrForwardMessageBundle(
+        bytes32 bundleRoot,
+        uint256 bundleValue,
+        uint256 bundleFees,
+        uint256 fromChainId,
+        uint256 toChainId,
+        uint256 commitTime
+    )
+        external
+        payable
+    {
+        // Nonreentrant
+        // distribute bundle reward if msg.sender == bundleRelayerAddress || block.timestamp > (commitTime + protectedRelayTime)
+
+        if (toChainId == getChainId()) {
+            bytes32 bundleId = keccak256(abi.encodePacked(bundleRoot, bundleValue, toChainId));
+            bundles[bundleId] = ConfirmedBundle(bundleRoot, bundleValue);
+        } else {
+            ISpokeMessageBridge spokeBridge = bridgeForChainId[toChainId];
+            spokeBridge.receiveMessageBundle(bundleRoot, bundleValue, fromChainId);
+        }
+
+        uint256 relayWindowStart = commitTime + exitTime[fromChainId];
+        uint256 relayWindowEnd = relayWindowStart + relayWindow;
+        uint256 relayReward = 0;
+        if (block.timestamp > relayWindowEnd) {
+            relayReward = bundleFees;
+        } else if (block.timestamp >= relayWindowStart) {
+            relayReward = (block.timestamp - relayWindowStart) * bundleFees / relayWindow;
+        }
+
+        if (relayReward > 0) {
+            transfer(tx.origin, relayReward)
+        }
+    }
+
+    function transfer(address to, uint256 amount) private {
+        (bool success, ) = tx.origin.call{value}
+        require(success, "BRG: Transfer failed")
+    }
+}
