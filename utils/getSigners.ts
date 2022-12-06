0df64ac1ae52adbf98125fd59c17fa8658e98212
cwhinfrey <cwhinfrey@gmail.com>
2022-12-06 15:26:16 -0800

    Separate out utils

---
+++ utils/getSigners.ts
@@ -0,0 +1,23 @@
+import * as ethers from 'ethers'
+
+function getSigners() {
+  const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY
+  if (!deployerPrivateKey) {
+    throw new Error('Missing environment variable DEPLOYER_PRIVATE_KEY')
+  }
+
+  const hubRpc = process.env.RPC_ENDPOINT_GOERLI ?? ''
+  const hubProvider = new ethers.providers.JsonRpcProvider(hubRpc)
+  const hubSigner = new ethers.Wallet(deployerPrivateKey, hubProvider)
+
+  const spokeRpc = process.env.RPC_ENDPOINT_OPTIMISM_GOERLI ?? ''
+  const spokeProvider = new ethers.providers.JsonRpcProvider(spokeRpc)
+  const spokeSigner = new ethers.Wallet(deployerPrivateKey, spokeProvider)
+
+  return {
+    hubSigner,
+    spokeSigners: [spokeSigner],
+  }
+}
+
+export default getSigners
