9a581469354522092948c69ff781ec0d25014917
cwhinfrey <cwhinfrey@gmail.com>
2022-12-07 13:52:46 -0800

    No need to reserve MSB

--- contracts/libraries/Bitmap.sol
+++ contracts/libraries/Bitmap.sol
@@ -10,23 +10,23 @@ library BitmapLibrary {
     error AlreadyFalse(uint256 index, uint256 chunkIndex, uint256 bitOffset);
 
     function isTrue(Bitmap storage bitmap, uint256 index) internal view returns (bool) {
-        uint256 chunkIndex = index / 255; // Note: Reserves the MSB.
-        uint256 bitOffset = index % 255;
+        uint256 chunkIndex = index / 256;
+        uint256 bitOffset = index % 256;
         bytes32 bitmapChunk = bitmap._bitmap[chunkIndex];
         return _isTrue(bitmapChunk, bitOffset);
     }
 
     function switchTrue(Bitmap storage bitmap, uint256 index) internal {
-        uint256 chunkIndex = index / 255; // Note: Reserves the MSB.
-        uint256 bitOffset = index % 255;
+        uint256 chunkIndex = index / 256;
+        uint256 bitOffset = index % 256;
         bytes32 bitmapChunk = bitmap._bitmap[chunkIndex];
         if (_isTrue(bitmapChunk, bitOffset)) revert AlreadyTrue(index, chunkIndex, bitOffset);
         bitmap._bitmap[chunkIndex] = (bitmapChunk | bytes32(1 << bitOffset));
     }
 
     function switchFalse(Bitmap storage bitmap, uint256 index) internal {
-        uint256 chunkIndex = index / 255; // Note: Reserves the MSB.
-        uint256 bitOffset = index % 255;
+        uint256 chunkIndex = index / 256;
+        uint256 bitOffset = index % 256;
         bytes32 bitmapChunk = bitmap._bitmap[chunkIndex];
         if (!_isTrue(bitmapChunk, bitOffset)) revert AlreadyFalse(index, chunkIndex, bitOffset);
         bitmap._bitmap[chunkIndex] = (bitmapChunk & ~bytes32(1 << bitOffset));
