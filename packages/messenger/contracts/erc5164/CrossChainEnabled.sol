6beb07abae5a68723c7a5558d59941531c6d67e8
cwhinfrey <cwhinfrey@gmail.com>
2023-01-23 11:43:25 -0800

    Reformat _crossChainContext

--- packages/messenger/contracts/erc5164/CrossChainEnabled.sol
+++ packages/messenger/contracts/erc5164/CrossChainEnabled.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.2;
 
 contract CrossChainEnabled {
-    function _crossChainContext() internal pure returns (bytes32, uint256, address) {
-        bytes32 messageId;
-        uint256 fromChainId;
-        address from;
+    function _crossChainContext()
+        internal
+        pure
+        returns (bytes32 messageId, uint256 fromChainId, address from)
+    {
         assembly {
             messageId := calldataload(sub(calldatasize(), 84))
             fromChainId := calldataload(sub(calldatasize(), 52))
             from := shr(96, calldataload(sub(calldatasize(), 20)))
         }
-        return (messageId, fromChainId, from);
     }
 }
