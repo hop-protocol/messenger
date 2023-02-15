9f2090c1c0637f1d4f37f8e74aac42b81084425b
cwhinfrey <cwhinfrey@gmail.com>
2023-02-14 17:37:34 -0800

    Add hub connector factory

--- packages/connectors/contracts/HubERC5164ConnectorFactory.sol
+++ packages/connectors/contracts/HubERC5164ConnectorFactory.sol
@@ -3,24 +3,56 @@
 pragma solidity ^0.8.2;
 
 import "./ERC5164ConnectorFactory.sol";
+import "@hop-protocol/ERC5164/contracts/ISingleMessageDispatcher.sol";
 
 contract HubERC5164ConnectorFactory is ERC5164ConnectorFactory {
     constructor(address _erc5164Messenger) ERC5164ConnectorFactory(_erc5164Messenger) {}
 
-    function deployConnector(
-        address target,
-        uint256 counterpartChainId,
-        address counterpartConnector,
-        address counterpartTarget // Only used for create2 salt
-    ) external override returns (address) {
-        _deployConnector(target, counterpartChainId, counterpartConnector, counterpartTarget);
+    function connect(
+        uint256 chainId1,
+        address target1,
+        uint256 chainId2,
+        address target2
+    )
+        external
+        returns (address)
+    {
+        address calculatedAddress = calculateAddress(chainId1, target1, chainId2, target2);
+
+        triggerDeployment(calculatedAddress, chainId1, target1, chainId2, target2);
+        triggerDeployment(calculatedAddress, chainId2, target2, chainId1, target1);
+
+        return calculatedAddress;
     }
 
-    function _deployConnector(
+    function triggerDeployment(
+        address calculatedAddress,
+        uint256 chainId,
         address target,
-        uint256 counterpartChainId,
-        address counterpartConnector,
-        address counterpartTarget // Only used for create2 salt
-    ) internal override returns (address) {
+        uint256 couterpartChainId,
+        address counterpartTarget
+    )
+        internal
+    {
+        if (chainId == getChainId()) {
+            address connector = _deployConnector(
+                target,
+                couterpartChainId,
+                calculatedAddress,
+                counterpartTarget
+            );
+            assert(calculatedAddress == connector);
+        } else {
+            ISingleMessageDispatcher(erc5164Messenger).dispatchMessage(
+                chainId,
+                address(this),
+                abi.encodeWithSignature(
+                    "deployConnector(address,uint256,address,address)",
+                    target,
+                    couterpartChainId,
+                    counterpartTarget
+                )
+            );
+        }
     }
 }
