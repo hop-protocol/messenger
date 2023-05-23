210e2550bbe45163d51c1a3dd0db2d33cbc84615
cwhinfrey <cwhinfrey@gmail.com>
2023-05-22 17:05:16 -0700

    remove old contracts

--- packages/messenger/old/BundleTransporter/IBundleTransporter.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.2;
-
-interface IBundleTransporter {
-    function transportBundle(bytes32 bundleId, bytes32 bundleRoot, uint256 toChainId) external payable;
-    function bundleIsValid(bytes32 bundleId, bytes32 bundleRoot, uint256 fromChainId) external view returns (bool);
-}
