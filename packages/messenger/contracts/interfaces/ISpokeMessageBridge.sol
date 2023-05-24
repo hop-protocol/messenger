9748f1b82997fd4d75ee135353d24c2a141f1d9a
cwhinfrey <cwhinfrey@gmail.com>
2023-05-23 19:01:09 -0700

    remove interfaces

--- packages/messenger/contracts/interfaces/ISpokeMessageBridge.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.2;
-
-interface ISpokeMessageBridge {
-    function receiveMessageBundle(bytes32 bundleId, bytes32 bundleRoot, uint256 fromChainId) external;
-    function forwardMessage(bytes32 messageId, address from, address to, bytes calldata data) external;
-}
