b35dcecbac4a1d7768bd97c1c7716a7dd80ea3ab
cwhinfrey <cwhinfrey@gmail.com>
2023-07-10 13:41:26 -0700

    move external connectors

--- packages/messenger/contracts/imports.sol
+++ packages/messenger/contracts/imports.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.2;
 
 import "@hop-protocol/connectors/contracts/test/MockConnector.sol";
-import "@hop-protocol/connectors/contracts/L1ArbitrumConnector.sol";
-import "@hop-protocol/connectors/contracts/L2ArbitrumConnector.sol";
-import "@hop-protocol/connectors/contracts/L1OptimismConnector.sol";
-import "@hop-protocol/connectors/contracts/L2OptimismConnector.sol";
-import "@hop-protocol/connectors/contracts/L1PolygonConnector.sol";
-import "@hop-protocol/connectors/contracts/L2PolygonConnector.sol";
+import "@hop-protocol/connectors/contracts/external/L1ArbitrumConnector.sol";
+import "@hop-protocol/connectors/contracts/external/L2ArbitrumConnector.sol";
+import "@hop-protocol/connectors/contracts/external/L1OptimismConnector.sol";
+import "@hop-protocol/connectors/contracts/external/L2OptimismConnector.sol";
+import "@hop-protocol/connectors/contracts/external/L1PolygonConnector.sol";
+import "@hop-protocol/connectors/contracts/external/L2PolygonConnector.sol";
