783c9e1b03ce653e7cc4c7557ac6435390158696
cwhinfrey <cwhinfrey@gmail.com>
2023-07-13 11:58:31 -0700

    nonce normalization for any gap

--- packages/connectors/scripts/normalizeNonce.ts
+++ packages/connectors/scripts/normalizeNonce.ts
@@ -1,24 +1,35 @@
-import { Contract, Signer, ContractTransaction } from 'ethers'
-import { ethers } from 'hardhat'
-import { getSigners, logContractDeployed } from '../utils'
-import { hopMessengers } from './config'
+import { Wallet } from 'ethers'
+import { getSigners } from '../utils'
 
 async function main() {
-  const hubChainId = '5'
-  const spokeChainId = '420'
+  console.log('Normalizing nonces')
+  const signerObject = getSigners()
 
-  const signers = getSigners()
-  const hubSigner = signers[hubChainId]
-  const spokeSigner = signers[spokeChainId]
-
-  for (let i = 0; i < 15; i++) {
-    const tx = await spokeSigner.sendTransaction({to: spokeSigner.address})
-    await tx.wait()
+  const chains: string[] = []
+  const signers: Wallet[] = []
+  const nonces: number[] = []
+  let highestNonce = 0
+  for (const chainId in signerObject) {
+    const signer = signerObject[chainId]
+    const nonce = await signer.getTransactionCount()
+    chains.push(chainId)
+    signers.push(signer)
+    nonces.push(nonce)
+    if (nonce > highestNonce) {
+      highestNonce = nonce
+    }
+    console.log(`Chain ${chainId} nonce: ${nonce}`)
   }
 
-  for (let i = 0; i < 0; i++) {
-    const tx = await hubSigner.sendTransaction({to: hubSigner.address})
-    await tx.wait()
+  for (let i = 0; i < chains.length; i++) {
+    const nonceGap = highestNonce - nonces[i]
+    console.log(`Chain ${chains[i]} nonce gap: ${nonceGap}`)
+    const signer = signers[i]
+    for (let j = 0; j < nonceGap; j++) {
+      const tx = await signer.sendTransaction({to: signer.address})
+      await tx.wait()
+      process.stdout.write('.')
+    }
   }
 }
 
